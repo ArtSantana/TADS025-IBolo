6713ca384bcac4e766fc1b72107a20d557c45181
Arthur Santana <arthur.santana@sensedia.com>
2021-05-15 19:01:36 -0300

    Finished database connection

--- domain/config/database.py
+++ domain/config/database.py
@@ -3,5 +3,15 @@ from pymongo import MongoClient
 
 DB_USER = config('DB_USER')
 DB_PASSWORD = config('DB_PASSWORD')
+DB_NAME = config('DB_NAME')
+CONNECTION_URI = 'mongodb://%s:%s@127.0.0.1' % (DB_USER, DB_PASSWORD)
 
-client = MongoClient('mongodb://%s:%s@127.0.0.1' % (DB_USER, DB_PASSWORD), 27017)
+class DatabaseControl:
+    def __init__(self):
+        self.client = MongoClient(CONNECTION_URI, 27017)
+        self.db = self.client[DB_NAME]
+
+    def close_connection(self):
+        self.client.close()
+    
+database = DatabaseControl()
