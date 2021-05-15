21b8ffeee0e029e3173eecfe6c66c2a3827b7ef5
Arthur Santana <arthur.santana@sensedia.com>
2021-05-15 19:01:58 -0300

    Finished ingredient CRUD

--- domain/ingredients/service.py
+++ domain/ingredients/service.py
@@ -1,15 +1,27 @@
 from domain.ingredients.models.ingredient import Ingredient
-from typing import Type, TypeVar
+from domain.ingredients.models.ingredient_db import IngredientDB
+from domain.ingredients.repository import RepositoryIngredient
+from typing import Type
 
 class ServiceIngredient:
-    def create_ingredient(self, ingredient: Type[Ingredient]):
-        print(ingredient.name, ingredient.unit, ingredient.price)
-    
-    def get_ingredients(self):
-        print('getting ingredients')
-    
-    def delete_ingredients(self, id: str):
-        print(id)
+    def __init__(self):
+        self.repository = RepositoryIngredient()
 
-    def update_ingredients(self, ingredient: Type[Ingredient]):
-        print(ingredient)
+    def create_ingredient(self, ingredient: Type[Ingredient]):
+        return self.repository.insert(ingredient)
+    
+    def get_all_ingredients(self):
+        list = self.repository.get_all()
+        obj_list = []
+        for document in list:
+            parsed_ingredient = IngredientDB()
+            parsed_ingredient.parseFromDict(document)
+            obj_list.append(parsed_ingredient.__dict__)
+        return obj_list
+
+    
+    def delete_ingredient(self, id: str):
+        return self.repository.delete(id)
+
+    def update_ingredients(self, ingredient: Type[Ingredient], id: str):
+        return self.repository.update_ingrendient(ingredient, id)
